b5566b8571f197434e4f5cb01012ec7d82876c93
vvenugopa <vaka_venugopalakrishna@premierinc.com>
2025-06-02 22:30:10 +0530

    Update verifyFeasibilityPlusReportExecution.cs

--- verifyFeasibilityPlusReportExecution.cs
+++ verifyFeasibilityPlusReportExecution.cs
@@ -5,7 +5,7 @@ public void VerifyFeasibilityPlusReportExecution()
 
     HomePage homePage = signInPage.Login(Username, Password);
 
-    /*PopulationsPage populationPage = homePage.ClickAndWaitBasePage(homePage.PopulationMenuLink);
+    PopulationsPage populationPage = homePage.ClickAndWaitBasePage(homePage.PopulationMenuLink);
 
     String datetime = DateTime.Now.ToString("yy-MM-dd HH:mm:ss").Replace("-", "").Replace(" ", "").Replace(":", "");
 
@@ -14,10 +14,11 @@ public void VerifyFeasibilityPlusReportExecution()
     String AttributeType = "Diagnoses";
     String Attribute = "ICD Code - Diagnoses";
     String FilterCoditionOne = "in";
-    String FilterConditionSearchText = "070.2";
+    String[] FilterConditionSearchText = { "070.21","070.22" };
+    String FilterSearchResultsSelection = "Individual";
     String IsContract = "Yes";
 
-    populationPage.CreateNewPopulation(Title, Description, AttributeType, Attribute, FilterCoditionOne, FilterConditionSearchText, IsContract);
+    populationPage.CreateNewPopulation(Title, Description, AttributeType, Attribute, FilterCoditionOne, FilterConditionSearchText, FilterSearchResultsSelection ,IsContract);
 
     // Validate first population status
     Assert.That(ElemGet.Grid_CellTextFound(Browser, populationPage.PopulationDetailsTbl, 5, "td", " Processing"));
@@ -27,11 +28,24 @@ public void VerifyFeasibilityPlusReportExecution()
     Browser.WaitJSAndJQuery();
 
     Browser.WaitForElement(Bys.PopulationsPageBy.ActionsHomeBtn, TimeSpan.FromSeconds(60), ElementCriteria.IsVisible);
-    */
+
+    // Open the created popoulation
+
+    populationPage.ClickAndWait(populationPage.populationDetailsTblFirtRowNameColumn);
+
+    // Retrive the Details of Visits, Population, Sites
+
+    String[] populationVisitCountText = Browser.FindElement(Bys.PopulationsPageBy.PopulationVisitCountlabel).Text.Split(':');
+    String[] populationPatientCountText = Browser.FindElement(Bys.PopulationsPageBy.PopulationPatientCountlabel).Text.Split(':');
+    String[] populationSiteCountText =   Browser.FindElement(Bys.PopulationsPageBy.PopulationHospitalCountlabel).Text.Split(':');
+
+    String populationVisitCount = populationVisitCountText[1].Trim();
+    String populationPatientCount = populationPatientCountText[1].Trim();
+    String populationSiteCount = populationSiteCountText[1].Trim();
 
     // Navigate to Report Menu
     ReportsPage reportsPage = homePage.ClickAndWaitBasePage(homePage.ReportMenuLink);
-    String population = "Feasibility Plus Report- Auto _check";
+    String population = Title;
 
     reportsPage.SelectSpecificReport(reportsPage.FeasibilityPlusSelectPopulationBtn, "No");
     reportsPage.SearchForAPopulation(population);
@@ -48,7 +62,11 @@ public void VerifyFeasibilityPlusReportExecution()
     Thread.Sleep(30);
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
-    Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblitySitesLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
+    Browser.WaitForElement(Bys.ReportsPageBy.FeasibilityPlusSitesLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
+
+    String reportVisitCount = Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel).Text;
+    String reportPatientCount = Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel).Text;
+    String reportSiteCount = Browser.FindElement(Bys.ReportsPageBy.FeasibilityPlusSitesLabel).Text;
 
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasibilityPopulationLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
     Thread.Sleep(TimeSpan.FromSeconds(5));
@@ -56,15 +74,15 @@ public void VerifyFeasibilityPlusReportExecution()
     // Visit , Patients, Sites Count Check
 
     // Visit Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel).Text.Contains("4,620"))
+    if (! reportVisitCount.Contains(populationVisitCount))
         throw new Exception("Visit count doesn't match with the population " + population + " as per in the summary tab");
 
     // Patients Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel).Text.Contains("3,572"))
+    if (!reportPatientCount.Contains(populationPatientCount))
         throw new Exception("Patient count doesn't match with the population " + population + " as per in the summary tab");
 
     // Sites Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblitySitesLabel).Text.Contains("592"))
+    if (! reportSiteCount.Contains(populationSiteCount))
         throw new Exception("Sites count doesn't match with the population " + population + " as per in the summary tab");
 
     // Validate in URL
@@ -80,23 +98,27 @@ public void VerifyFeasibilityPlusReportExecution()
     Thread.Sleep(30);
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
-    Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblitySitesLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
+    Browser.WaitForElement(Bys.ReportsPageBy.FeasibilityPlusSitesLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
 
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasibilityPopulationLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
     Thread.Sleep(TimeSpan.FromSeconds(5));
 
+    reportVisitCount = Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel).Text;
+    reportPatientCount = Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel).Text;
+    reportSiteCount = Browser.FindElement(Bys.ReportsPageBy.FeasibilityPlusSitesLabel).Text;
+
     // Visit , Patients, Sites Count Check
 
     // Visit Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel).Text.Contains("4,620"))
+    if (! reportVisitCount.Contains(populationVisitCount))
         throw new Exception("Visit count doesn't match with the population " + population + " as per in the Base Profile tab");
 
     // Patients Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FeasiblityPlusPatientsLabel).Text.Contains("3,572"))
+    if (! reportPatientCount.Contains(populationPatientCount))
         throw new Exception("Patient count doesn't match with the population " + population + " as per in the Base Profile tab");
 
     // Sites Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblitySitesLabel).Text.Contains("592"))
+    if (! reportSiteCount.Contains(populationSiteCount))
         throw new Exception("Sites count doesn't match with the population " + population + " as per in the Base Profile tab");
 
     reportsPage.ClickAndWait(reportsPage.PatientCharacteristicsTabBtn);
@@ -107,23 +129,27 @@ public void VerifyFeasibilityPlusReportExecution()
     Thread.Sleep(30);
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
-    Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblitySitesLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
+    Browser.WaitForElement(Bys.ReportsPageBy.FeasibilityPlusSitesLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
 
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasibilityPopulationLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
     Thread.Sleep(TimeSpan.FromSeconds(5));
 
+    reportVisitCount = Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel).Text;
+    reportPatientCount = Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel).Text;
+    reportSiteCount = Browser.FindElement(Bys.ReportsPageBy.FeasibilityPlusSitesLabel).Text;
+
     // Visit , Patients, Sites Count Check
 
     // Visit Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel).Text.Contains("4,620"))
+    if (! reportVisitCount.Contains(populationVisitCount))
         throw new Exception("Visit count doesn't match with the population " + population + " as per in the Patient Characteristics tab");
 
     // Patients Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel).Text.Contains("3,572"))
+    if (! reportPatientCount.Contains(populationPatientCount))
         throw new Exception("Patient count doesn't match with the population " + population + " as per in the Patient Characteristics tab");
 
     // Sites Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblitySitesLabel).Text.Contains("592"))
+    if (! reportSiteCount.Contains(populationSiteCount))
         throw new Exception("Sites count doesn't match with the population " + population + " as per in the Patient Characteristics tab");
 
     reportsPage.ClickAndWait(reportsPage.VisitCharacteristicsTabBtn);
@@ -133,23 +159,27 @@ public void VerifyFeasibilityPlusReportExecution()
 
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
-    Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblitySitesLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
+    Browser.WaitForElement(Bys.ReportsPageBy.FeasibilityPlusSitesLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
 
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasibilityPopulationLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
     Thread.Sleep(TimeSpan.FromSeconds(5));
 
+    reportVisitCount = Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel).Text;
+    reportPatientCount = Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel).Text;
+    reportSiteCount = Browser.FindElement(Bys.ReportsPageBy.FeasibilityPlusSitesLabel).Text;
+
     // Visit , Patients, Sites Count Check
 
     // Visit Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel).Text.Contains("4,620"))
+    if (! reportVisitCount.Contains(populationVisitCount))
         throw new Exception("Visit count doesn't match with the population " + population + " as per in the Visit Characteristics tab");
 
     // Patients Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel).Text.Contains("3,572"))
+    if (! reportPatientCount.Contains(populationPatientCount))
         throw new Exception("Patient count doesn't match with the population " + population + " as per in the Visit Characteristics tab");
 
     // Sites Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblitySitesLabel).Text.Contains("592"))
+    if (! reportSiteCount.Contains(populationSiteCount))
         throw new Exception("Sites count doesn't match with the population " + population + " as per in the Visit Characteristics tab");
 
     reportsPage.ClickAndWait(reportsPage.SiteCharacteristicsTabBtn);
@@ -159,23 +189,27 @@ public void VerifyFeasibilityPlusReportExecution()
 
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
-    Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasiblitySitesLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
+    Browser.WaitForElement(Bys.ReportsPageBy.FeasibilityPlusSitesLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
 
     Browser.WaitForElement(Bys.ReportsPageBy.FreeFeasibilityPopulationLabel, TimeSpan.FromSeconds(180), ElementCriteria.IsVisible);
     Thread.Sleep(TimeSpan.FromSeconds(5));
 
+    reportVisitCount = Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel).Text;
+    reportPatientCount = Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel).Text;
+    reportSiteCount = Browser.FindElement(Bys.ReportsPageBy.FeasibilityPlusSitesLabel).Text;
+
     // Visit , Patients, Sites Count Check
 
     // Visit Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityVisitsLabel).Text.Contains("4,620"))
+    if (! reportVisitCount.Contains(populationVisitCount))
         throw new Exception("Visit count doesn't match with the population " + population + " as per in the Site Characteristics tab");
 
     // Patients Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblityPatientsLabel).Text.Contains("3,572"))
+    if (! reportPatientCount.Contains(populationPatientCount))
         throw new Exception("Patient count doesn't match with the population " + population + " as per in the Site Characteristics tab");
 
     // Sites Count Check
-    if (!Browser.FindElement(Bys.ReportsPageBy.FreeFeasiblitySitesLabel).Text.Contains("592"))
+    if (! reportSiteCount.Contains(populationSiteCount))
         throw new Exception("Sites count doesn't match with the population " + population + " as per in the Site Characteristics tab");
 
     reportsPage.ClickAndWait(reportsPage.AppendicesTabBtn);
@@ -185,49 +219,49 @@ public void VerifyFeasibilityPlusReportExecution()
 
     Browser.WaitForElement(Bys.ReportsPageBy.AppendixACriteriaLabel, TimeSpan.FromSeconds(100), ElementCriteria.IsVisible);
 
-    String expectedCriteria = "Beginning of Population Criteria\r\nBeginning of Group [ Untitled ]\r\n[ICD Code - Diagnoses In 070.21 - HEPAT B ACUTE VIRAL W/COMA W/DELTA\r\n070.22 - HEPAT B CHRN VIRAL W/COMA W/O DELTA]\r\nEnd of Group [ Untitled ]\r\nEnd of Population Criteria";
+    String expectedCriteria = "Beginning of Population Criteria\r\nBeginning of Group [ Untitled ]\r\n[ICD Code - Diagnoses In 070.22 - HEPAT B CHRN VIRAL W/COMA W/O DELTA\r\n070.21 - HEPAT B ACUTE VIRAL W/COMA W/DELTA]\r\nEnd of Group [ Untitled ]\r\nEnd of Population Criteria";
     String displayedCriteria = Browser.FindElement(Bys.ReportsPageBy.AppendixACriteriaLabel).Text;
     if (!expectedCriteria.Equals(displayedCriteria))
         throw new Exception("Expected Criteria " + expectedCriteria + " doesn't match with displayed Criteria " + displayedCriteria + " for the Feasibility Plus Report related to population " + population);
 
-    // Browser.WaitForElement(Bys.ReportsPageBy.AppendixCSupportPageLink, TimeSpan.FromSeconds(100));
-    // Browser.WaitForElement(Bys.ReportsPageBy.AppendixCFeedbackLink, TimeSpan.FromSeconds(100));
+    Browser.WaitForElement(Bys.ReportsPageBy.AppendixCSupportPageLink, TimeSpan.FromSeconds(100));
+    Browser.WaitForElement(Bys.ReportsPageBy.AppendixCFeedbackLink, TimeSpan.FromSeconds(100));
 
-    //((IJavaScriptExecutor)Browser).ExecuteScript("arguments[0].scrollIntoView(true);", reportsPage.AppendixCFeedbackLink);
+   /* ((IJavaScriptExecutor)Browser).ExecuteScript("arguments[0].scrollIntoView(true);", reportsPage.AppendixCFeedbackLink);
     Thread.Sleep(TimeSpan.FromSeconds(5));
-    //reportsPage.ClickAndWait(reportsPage.AppendixCSupportPageLink);
-    // Thread.Sleep(TimeSpan.FromSeconds(5));
+    reportsPage.ClickAndWait(reportsPage.AppendixCSupportPageLink);
+    Thread.Sleep(TimeSpan.FromSeconds(5));
 
     // Get all windows
-    //ReadOnlyCollection<string> allWindows = Browser.WindowHandles;
+    ReadOnlyCollection<string> allWindows = Browser.WindowHandles;
 
-    //String parentWindow = allWindows.ElementAt(0); String childWindow = allWindows.ElementAt(1);
+    String parentWindow = allWindows.ElementAt(0); String childWindow = allWindows.ElementAt(1);
 
-    //Browser.SwitchTo().Window(childWindow);
-    //Browser.SwitchTo().DefaultContent();
-    //Browser.WaitForElement(Bys.ReportsPageBy.asdfadsf, TimeSpan.FromSeconds(100), ElementCriteria.IsVisible);
-
-    // Validate and Close the Window
-    //Browser.Close();
-
-    //Browser.SwitchTo().Window(parentWindow);
-
-    //Browser.SwitchTo().DefaultContent();
-    //Browser.SwitchTo().Frame(0);
-    //reportsPage.ClickAndWait(reportsPage.AppendixCFeedbackLink);
-
-    //allWindows = Browser.WindowHandles;
-
-    //parentWindow = allWindows.ElementAt(0); childWindow = allWindows.ElementAt(1);
-
-    //Browser.SwitchTo().Window(childWindow);
-    //Browser.SwitchTo().DefaultContent();
-    //Browser.WaitForElement(Bys.ReportsPageBy.asdfadsf, TimeSpan.FromSeconds(100), ElementCriteria.IsVisible);
+    Browser.SwitchTo().Window(childWindow);
+    Browser.SwitchTo().DefaultContent();
+    Browser.WaitForElement(Bys.ReportsPageBy.ProductSupportRequestLabel, TimeSpan.FromSeconds(100), ElementCriteria.IsVisible);
 
     // Validate and Close the Window
-    //Browser.Close();
+    Browser.Close();
 
-    //Browser.SwitchTo().Window(parentWindow);
+    Browser.SwitchTo().Window(parentWindow);
+
+    Browser.SwitchTo().DefaultContent();
+    Browser.SwitchTo().Frame(0);
+    reportsPage.ClickAndWait(reportsPage.AppendixCFeedbackLink);
+
+    allWindows = Browser.WindowHandles;
+
+    parentWindow = allWindows.ElementAt(0); childWindow = allWindows.ElementAt(1);
+
+    Browser.SwitchTo().Window(childWindow);
+    Browser.SwitchTo().DefaultContent();
+    Browser.WaitForElement(Bys.ReportsPageBy.ProductCustomerFeedbackLabel, TimeSpan.FromSeconds(100), ElementCriteria.IsVisible);
+
+    // Validate and Close the Window
+    Browser.Close();
+
+    Browser.SwitchTo().Window(parentWindow); */
 
     Browser.SwitchTo().DefaultContent();
     reportsPage.ClickAndWait(reportsPage.ExportReportbtn);
